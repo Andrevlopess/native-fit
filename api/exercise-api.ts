bb3bb4feee2d69515f339a474bf5554732645fd4
andrevlopess <andrellopes021@gmail.com>
2024-08-05 08:35:22 -0300

    start: add series api

--- api/exercise-api.ts
+++ api/exercise-api.ts
@@ -1,3 +1,4 @@
+import { SerieValues } from "@/components/exercise/working-out/SeriesManager";
 import { supabase } from "@/lib/supabase";
 import { IExercise } from "@/types/exercise";
 
@@ -12,8 +13,14 @@ interface FindOneParams {
   id: string;
 }
 
+
+export interface AddSerieParams {
+  workout_id: string;
+  exercise_id: string;
+  series: SerieValues
+}
 export class ExerciseApi {
-  private constructor() {}
+  private constructor() { }
 
   static async findOne(params: FindOneParams) {
     try {
@@ -45,4 +52,77 @@ export class ExerciseApi {
       throw error;
     }
   }
+
+  static async addSerie(params: AddSerieParams) {
+    try {
+
+      const max_weight = params.series.serie.sort((a, b) => a.weight - b.weight);
+      const min_weight = params.series.serie.sort((a, b) => b.weight - a.weight);
+      const max_reps = params.series.serie.sort((a, b) => a.reps - b.reps);
+      const min_reps = params.series.serie.sort((a, b) => b.reps - a.reps);
+
+
+      // const { data, error } = await supabase
+      //   .from('exercises_series')
+        // .insert()
+
+
+
+      console.log(params.series);
+
+    } catch (error) {
+      throw error
+    }
+  }
 }
+
+const exercises_series = [
+  {
+  exercise_id: 'tal',
+  series: [
+    {
+      weight: 10,
+      reps: 5
+    },
+    {
+      weight: 12,
+      reps: 4
+    },
+    {
+      weight: 10,
+      reps: 8
+    },
+    {
+      weight: 14,
+      reps: 5
+    },
+  ]
+},
+  {
+  exercise_id: 'tal2',
+  series: [
+    {
+      weight: 150,
+      reps: 15
+    },
+    {
+      weight: 200,
+      reps: 10
+    },
+    {
+      weight: 240,
+      reps: 8
+    },
+    {
+      weight: 300,
+      reps: 6
+    },
+  ]
+},
+]
+exercises_series.map(exercise_serie => ({
+    execise_id: exercise_serie.exercise_id,
+    max_weight_serie: exercise_serie.series.sort((a,b) => b.weight - a.weight)[0],
+    min_weight_serie: exercise_serie.series.sort((a,b) => a.weight - b.weight)[0],
+    best_serie: exercise_serie.series.sort((a,b) => (b.weight / b.reps) - (a.weight / a.reps))[0]
+}))
--- components/exercise/working-out/SeriesManager.tsx
+++ components/exercise/working-out/SeriesManager.tsx
@@ -1,28 +1,60 @@
 import { ControlledInput } from '@/components/controllers/ControlledInput';
 import Button from '@/components/ui/Button';
+import COLORS from '@/constants/Colors';
 import { s } from '@/styles/global';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { X } from 'lucide-react-native';
 import React from 'react';
 import { useForm, useFieldArray } from 'react-hook-form';
 import { Text, View } from 'react-native';
+import { z } from 'zod';
+
+
+const SerieSchema = z.object({
+    serie: z.object({
+        weight: z.number(),
+        reps: z.number()
+    }).array()
+})
+
+export type SerieValues = z.infer<typeof SerieSchema>
 
 export default function SeriesManager() {
 
-    const { control, register } = useForm();
+    const { control, register } = useForm<SerieValues>({
+        resolver: zodResolver(SerieSchema),
+        defaultValues: {
+            serie: [{ }]
+        }
+    });
     const { fields, append, prepend, remove, swap, move, insert } = useFieldArray({
-        control, 
-        name: "serie", 
+        control,
+        name: "serie",
     });
 
     return (
         <View style={[s.flex1, s.p12, s.flexRow, s.justifyBetween, s.itemsCenter]}>
             <Text style={[s.bold, s.textXL, s.px12]}>Séries</Text>
-            <Button variant='secondary' text='Nova série' size='small' onPress={() => { }} />
+            <Button variant='secondary' text='Nova série' size='small' onPress={() => append({reps: 0, weight: 0})} />
 
             {fields.map((field, index) =>
-                <ControlledInput
-                    name={`serie.${index}.`}
-                    control={control}
-                />)}
+                <View key={field.id}>
+                    <ControlledInput
+                        name={`serie.${index}.weight`}
+                        control={control}
+                        value={field.weight.toString()}
+                    />
+
+                    <ControlledInput
+                        name={`serie.${index}.reps`}
+                        control={control}
+                        value={field.reps.toString()}
+                    />
+                    <Button onPress={() => remove(index)}>
+                        <X color={COLORS.red}/>
+                    </Button>
+                </View>
+            )}
         </View>
     )
 }
